585c4c5d2fd06ea49be1a8f097294fdb58cb6446
Austin Stewart <astewart@drndata.com>
2016-02-29 12:09:22 -0600

    Wrapped the app.js in an anonymous function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
+(function(){
+    "use strict";
 // Require the edmunds service
 require('./edmunds');
 
 var app = angular.module('plnkrApp', ['ngMaterial', 'edmundsApi']);
 app
   .controller("VINController", require('./controllers/VinController.js'));
+
+ })();
